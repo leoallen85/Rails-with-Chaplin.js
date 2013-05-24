cb9c9ec043e9261964ddb47c8dad603d593c21ee
Leo Allen <leo.f.allen@gmail.com>
2013-05-24 12:53:24 +0200

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Yoga::Application.config.secret_token = '58007ea0629e67f46a4d02fae0b1c77b081f9bcfcee5ab118fbd712309fbd7f6c6cb7dcf219cb562c10528a585d18d3c30e60c57e7250b0128df512790c21077'
+Yoga::Application.config.secret_token = 'xxx'
